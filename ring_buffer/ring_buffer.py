ad0b263bd90422b37a5e2c4c6b68450443525a3c
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-09-25 22:41:39 -0400

    :fire: Completed ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,23 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
+        self.oldest = 0
 
     def append(self, item):
-        pass
+        # if buffer is full,
+        # overwrite the oldest item with the new one
+        if len(self.data) == self.capacity:
+            self.data[self.oldest] = item
+        else:
+            self.data.append(item)
+        self.update_oldest()
 
     def get(self):
-        pass
+        return self.data
+
+    def update_oldest(self):
+        if self.oldest + 1 < self.capacity:
+            self.oldest += 1
+        else:
+            self.oldest = 0
